232590080aed8f8a4ec24b01ac85bb402ef11489
Faith Sabu <faithsabu1791@gmail.com>
2024-01-01 21:17:37 +0530

    nested elements without JSX

--- App.js
+++ App.js
@@ -1,7 +1,50 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React from App.js !!!"
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// Nested structure without JSX - The core of react
+
+// To do
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1>I'm an h1 tag</h1>
+  </div>
+</div> */
+}
+
+const element = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm an h1 tag")
+  )
+);
+
+root.render(element);
+
+// To Do
+{
+  /* <div id="parent">
+  <div id="child1">
+    <h1>I'm an h1 tag</h1>
+    <h2>I'm an h2 tag</h2>
+  </div>
+  <div id="child2">
+    <h1>I'm an h1 tag</h1>
+    <h2>I'm an h2 tag</h2>
+  </div>
+</div>; */
+}
+
+const element2 = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]); // give siblings in an array
+
+root.render(element2);
