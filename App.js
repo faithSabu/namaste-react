ec7922943e1636b128abc1c9706cd53514171589
Faith Sabu <faithsabu1791@gmail.com>
2024-01-04 16:12:44 +0530

    component vs element

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => <h1>This is Title component</h1>;
+const TitleComponent = () => (
+  <h1 id="titleComponent">This is Title component</h1>
+);
 
-// Eg. of Componet composition - ie, just combining mutliple componets (Here <Title/> component is used in another component)
-const HeaderComponent = () => (
-  <div id="container">
-    <Title /> 
-    This is Header component
+const bodyElement = <h3>This is body element</h3>;
+
+// How to use component (use angular brackets) and element (use curley brackets) in JSX.
+// How to use an element and component inside another element
+const titleElement = (
+  <div>
+    <h2 id="titleElement">This is Title Element</h2>
+    <TitleComponent />
+    {bodyElement}
+
+    {/* <TitleComponent /> & {TitleComponent()} => these are same; since functional component is
+    basically a js function. And we can use "{}" to execute js. So we can call a
+    function inside "{}".
+    So these are same:
+    1. <TitleComponent /> 
+    2. <TitleComponent></TitleComponent>
+    3. {TitleComponent()}
+    */}
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeaderComponent />); // use angular brackets to render a component
+root.render(titleElement); // rendering an element
